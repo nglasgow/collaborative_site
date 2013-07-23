445552592c17ec0edb1812a5c80108afb139c9d5
nglasgow <nicole.glasgow@ymail.com>
2013-07-22 23:13:50 -0700

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollaborativeSite::Application.config.secret_token = '87dc240a6c6e6a388d89c9aefbc799173ad0459f1cf508d3454826043a65f4d0f27bbc0f32b7413360022eb7dd9bc0fbbd4e7202d23249ffdb7a55af255eb6ff'
